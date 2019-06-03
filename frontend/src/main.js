5ed62fcc4a554b0257522e08bbd04f6c0a3360c8
aalluinmar <aravind.allu@inmar.com>
2019-06-03 22:00:17 +0530

    main.js importing Authorization header'

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import iView from 'iview';
+import axios from 'axios';
 import 'iview/dist/styles/iview.css';
 import locale from 'iview/dist/locale/en-US';
 import BootstrapVue from 'bootstrap-vue';
@@ -9,7 +10,12 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-
+window.axios = axios;
+window.token = localStorage.getItem('token');
+window.user = localStorage.getItem('user');
+// eslint-disable-next-line no-template-curly-in-string
+axios.defaults.headers.common.Authorization = 'Bearer ${  window.token}';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 // import axios from 'axios';
 
 Vue.use(BootstrapVue);
