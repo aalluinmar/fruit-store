803c79e985eac5049bb25e0822c858f83009c229
aalluinmar <aravind.allu@inmar.com>
2019-06-03 12:36:40 +0530

    some changes like indendation

--- backend/db.py
+++ backend/db.py
@@ -4,11 +4,12 @@ import os
 cnx = None
 cursor = None
 
+
 class DB(object):
     def __init__(self):
         cnx = connection.MySQLConnection(user=os.environ.get('USER_NAME'), password=os.environ.get('PASSWORD'),
-                                host='127.0.0.1',
-                                database='nodejs_login1')
+                                         host='127.0.0.1',
+                                         database='nodejs_login1')
         self.cursor = cnx.cursor()
         self.cnx = cnx
 
@@ -16,9 +17,11 @@ class DB(object):
         self.cursor.execute(statement)
         rows = self.cursor.fetchall()
         return rows
+
     def query_insert(self, statement):
         self.cursor.execute(statement)
         self.cnx.commit()
-    def close_connection(self):    
+
+    def close_connection(self):
         self.cursor.close()
         self.cnx.close()
