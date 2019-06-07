865bbe64985c3da43f46c8ad7429d35fffacb73a
ngutti <neeraja.gutti@inmar.com>
2019-06-07 16:44:30 -0700

    Added JWT Tokens

--- inmar/src/main.js
+++ inmar/src/main.js
@@ -4,6 +4,14 @@ import VModal from 'vue-js-modal';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import axios from 'axios';
+
+window.axios = axios;
+window.token = localStorage.getItem('token');
+window.user = localStorage.getItem('user');
+// eslint-disable-next-line no-template-curly-in-string
+axios.defaults.headers.common.Authorization = 'Bearer ${ window.token}';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 // import axios from 'axios';
 
 
