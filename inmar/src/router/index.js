533eb3bf566f3320fed8cbf5f79b271da4d31926
ngutti <neeraja.gutti@inmar.com>
2019-06-07 16:46:47 -0700

    Added Tokens

--- inmar/src/router/index.js
+++ inmar/src/router/index.js
@@ -6,6 +6,15 @@ import PurchaseItems from '@/components/PurchaseItems';
 
 Vue.use(Router);
 
+function requireAuth(to, from, next) {
+  if (localStorage.getItem('token')) {
+  next(); // we are authorized, continue on to the requested route
+  } else {
+  next('/'); // they are not authorized, so redirect to login
+  }
+  }
+  
+
 export default new Router({
   mode: 'history',
   routes: [
@@ -18,11 +27,13 @@ export default new Router({
       path: '/Sellitems',
       name: 'Sellitems',
       component: Sellitems,
+      beforeEnter: requireAuth,
     },
     {
       path: '/PurchaseItems',
       name: 'PurchaseItems',
       component: PurchaseItems,
+      beforeEnter: requireAuth,
     },
   ],
 });
